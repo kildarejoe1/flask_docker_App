02e9638677e0a711bc82714c07cf165d37b30111
henry morrin <henry.morrin@iii.com>
2019-12-20 23:28:13 +0000

    heroku login

--- blog/app.py
+++ blog/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+port = int(os.environ.get("PORT", 5000))
 
 
 app = Flask("__name__")
@@ -9,4 +10,4 @@ def index():
     return "<html> <title> Blog app </title> <body> Welcome to this flask app </body></html>"
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", port=port)
