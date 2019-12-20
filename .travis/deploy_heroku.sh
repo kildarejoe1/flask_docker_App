7d4b4c4b843e411739b569194c5a8c3dae629886
henry morrin <henry.morrin@iii.com>
2019-12-20 21:41:33 +0000

    heroku login

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -1,1 +1,5 @@
-test_pass1
+#!/bin/sh
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+heroku container:login
+heroku container:push web --app $HEROKU_APP_NAME
+heroku container:release web --app $HEROKU_APP_NAME
