46e358844b748e788f76053d8c1a08f42a4bca29
henry morrin <henry.morrin@iii.com>
2019-12-23 13:32:54 +0000

    Add jinja templates to web app

--- blog/app.py
+++ blog/app.py
@@ -1,14 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
+
 import os
 port = int(os.environ.get("PORT", 5000))
 
-
+posts = [ {"author" : "henry", "date": "11th Jnauary"}, { "author" : "james", "date": "4th Jamuary"}]
 app = Flask("__name__")
 
 
 @app.route("/")
+@app.route("/home")
 def index():
-    return "<html> <title> Blog app </title> <body> I just changed my flask app and deployed,but Welcome to this flask app </body></html>"
+    return render_template("index.html")
+
+@app.route("/posts")
+def display_posts():
+    return render_template("posts.html", posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=port)
